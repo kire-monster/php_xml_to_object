aa0a2316b0fefa252925a594c8164c687b5f309b
kire-monster <hell-dxd@hotmail.com>
2018-11-15 14:34:25 -0600

    pruebas del proyecto

---
+++ test/test.php
@@ -0,0 +1,29 @@
+<?php 
+/**
+ * Ejemplo
+ */
+include '../XMLObject.php';
+
+
+function fetchXML($xml)
+{
+	foreach ($xml as $value) {
+		printf("%s: %s",$value->name ,($value->value!=null)?$value->value:"null");
+		if($value->attributes!=null)
+		{
+			foreach ($value->attributes as $key => $attr) { printf("<br>%s => %s",$key,$attr); }
+		}
+		echo "<br><br>....................................................................................<br>";
+		if($value->children!=null){	fetchXML($value->children); }
+	}
+}
+
+
+$xml = file_get_contents("test.xml");
+$object = xml_to_object($xml);
+
+
+fetchXML($object->children);
+
+
+?>
